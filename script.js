a1c129ab0499e265390b093bea7b08318f720cbc
Nahid1818 <nahidislam12093487@gmail.com>
2024-03-30 22:21:24 +0600

    script fixed

--- script.js
+++ script.js
@@ -24,129 +24,134 @@ function numberToWordConverter() {
 
     } else {
 
-        // ------------Define arrays for number words------------
+        // Define arrays for number words
         let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        let teens = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
         let tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
         let thousands = ['', 'thousand', 'million', 'billion', 'trillion'];
 
+        // Function to convert a number less than 1000 to words
+        function convertLessThanOneThousand(number) {
+            let numStr = '';
 
-        let number = input.value;
+            if (number % 100 < 10) {
 
-        number = number.split("");
+                numStr += ones[number % 10];
+                number = Math.floor(number / 10);
 
-        mainfunction(number);
-        lastWork(number);
+            } else if (number % 100 < 20) {
 
-        function mainfunction(currentArray) {
-            switch (currentArray.length) {
-                case 1:
-                    currentArray[0] = ones[currentArray[0]];
-                    break;
-                case 2:
-                    currentArray[0] = tens[currentArray[0]];
-                    currentArray[1] = ones[currentArray[1]];
-                    break;
-                case 3:
-                    currentArray[0] = ones[currentArray[0]] + " hundred";
-                    currentArray[1] = tens[currentArray[1]];
-                    currentArray[2] = ones[currentArray[2]];
-                    break;
-                case 4:
-                    currentArray[0] = tens[currentArray[0]] + " thousand";
-                    currentArray[1] = ones[currentArray[1]] + " hundred";
-                    currentArray[2] = tens[currentArray[2]];
-                    currentArray[3] = ones[currentArray[3]];
-                    break;
-                case 5:
-                    currentArray[0] = tens[currentArray[0]];
-                    currentArray[1] = ones[currentArray[1]] + " thousand";
-                    currentArray[2] = ones[currentArray[2]] + " hundred";
-                    currentArray[3] = tens[currentArray[3]];
-                    currentArray[4] = ones[currentArray[4]];
-                    break;
-                case 6:
-                    currentArray[0] = ones[currentArray[0]] + " million";
-                    currentArray[1] = tens[currentArray[1]];
-                    currentArray[2] = ones[currentArray[2]] + " thousand";
-                    currentArray[3] = ones[currentArray[3]] + " hundred";
-                    currentArray[4] = tens[currentArray[4]];
-                    currentArray[5] = ones[currentArray[5]];
-                    break;
-                case 7:
-                    currentArray[0] = tens[currentArray[0]];
-                    currentArray[1] = ones[currentArray[1]] + " million";
-                    currentArray[2] = tens[currentArray[2]];
-                    currentArray[3] = ones[currentArray[3]] + " thousand";
-                    currentArray[4] = ones[currentArray[4]] + " hundred";
-                    currentArray[5] = tens[currentArray[5]];
-                    currentArray[6] = ones[currentArray[6]];
-                    break;
-                default:
-                    let number_extra = [];
-                    for (let i = 0; currentArray.length > 7; i++) {
+                numStr += teens[number % 10];
+                number = Math.floor(number / 100);
 
-                        if (currentArray.length % 7 === 0) {
+            } else {
 
-                            if (currentArray.length !== 7) {
+                numStr += ones[number % 10];
+                number = Math.floor(number / 10);
 
-                                number_extra[i] = currentArray.splice(0, 7);
-                            }
+                numStr = tens[number % 10] + " " + numStr;
+                number = Math.floor(number / 10);
 
-
-                        } else if (currentArray.length % 7 !== 0) {
-
-                            number_extra[i] = currentArray.splice(0, currentArray.length % 7);
-
-                        } else {
-                            console.log("something worng");
-                        }
-                    }
-
-                    number_extra.forEach((value) => {
-                        mainfunction(value);
-                    });
-
-                    mainfunction(number);
-
-
-                    number_extra.forEach((value) => {
-                        value.push("billion");
-                    });
-
-                    number = [number_extra, number].flat(2);
-                    break;
             }
+
+            if (number > 0) {
+
+                numStr = (number >= 10) ? ones[Math.floor(number / 10)] + ' hundred ' + numStr : ones[number] + ' hundred ' + numStr;
+
+            }
+
+            return numStr;
         }
 
-        function lastWork(lastTask) {
+        // Main function to convert a number to words
+        function convertNumberToWords(number) {
+            if (number === 0) {
+                return 'zero';
+            }
 
-            lastTask = lastTask.join(" ");
-            //1 to 19
-            lastTask = lastTask.replace("ten one", "eleven");
-            lastTask = lastTask.replace("ten two", "twelve");
-            lastTask = lastTask.replace("ten three", "thirteen");
-            lastTask = lastTask.replace("ten four", "fourteen");
-            lastTask = lastTask.replace("ten five", "fifteen");
-            lastTask = lastTask.replace("ten six", "sixteen");
-            lastTask = lastTask.replace("ten seven", "seventeen");
-            lastTask = lastTask.replace("ten eight", "eighteen");
-            lastTask = lastTask.replace("ten nine", "nineteen");
-            lastTask = lastTask.replace(",", " ");
+            let numStr = '';
+            let i = 0;
 
-            output.innerText = lastTask;
+            do {
+                let chunk = number % 1000;
+                if (chunk !== 0) {
+                    numStr = convertLessThanOneThousand(chunk) + " " + thousands[i] + ' ' + numStr;
+                }
+                number = Math.floor(number / 1000);
+                i++;
+            } while (number > 0);
+
+            return numStr.trim();
         }
+
+        // Example usage
+        let number = input.value;
+        let words = convertNumberToWords(number);
+        output.innerText = words;
 
 
     }
 
 }
 
+function wordToNumberConverter() {
+    // Define arrays for number words
+    let onesMap = {
+        'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
+        'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+    };
+    let teensMap = {
+        'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15,
+        'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19
+    };
+    let tensMap = {
+        'ten': 10, 'twenty': 20, 'thirty': 30, 'forty': 40, 'fifty': 50,
+        'sixty': 60, 'seventy': 70, 'eighty': 80, 'ninety': 90
+    };
+    let thousandsMap = {
+        'thousand': 1000, 'million': 1000000, 'billion': 1000000000, 'trillion': 1000000000000
+    };
 
+    // Function to convert words to numbers
+    function convertWordsToNumber(words) {
+        let wordArray = words.toLowerCase().split(' ');
 
-function wordToNumberConverter() { }
+        let number = 0;
+        let chunk = 0;
+
+        for (let i = 0; i < wordArray.length; i++) {
+            let word = wordArray[i];
+            if (onesMap[word] !== undefined) {
+                chunk += onesMap[word];
+            }
+            else if (teensMap[word] !== undefined) {
+                chunk += teensMap[word];
+            }
+            else if (tensMap[word] !== undefined) {
+                chunk += tensMap[word];
+            }
+            else if (thousandsMap[word] !== undefined) {
+                number += chunk * thousandsMap[word];
+                chunk = 0;
+            }
+            else if (word === 'hundred') {
+                chunk *= 100;
+            }
+        }
+
+        number += chunk;
+
+        return number;
+    }
+
+    // Example usage
+    let words = input.value;
+    let number = convertWordsToNumber(words);
+    output.innerText = number;
+
+}
 
 function ResetAll() {
     input.value = "";
     output.innerText = "Output";
 }
-
